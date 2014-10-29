11fa29958a67419115b1ac936060d5212506948b
rockyraccoon20 <tim_montgomery20@yahoo.com>
2014-10-29 18:52:06 -0400

    opinions table
    added opinions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029215444) do
+ActiveRecord::Schema.define(version: 20141029224713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20141029215444) do
   create_table "artists", force: true do |t|
     t.string "name"
     t.string "image_url"
+  end
+
+  create_table "opinons", force: true do |t|
+    t.integer "user_id"
+    t.integer "artist_id"
+    t.boolean "like"
   end
 
   create_table "users", force: true do |t|
