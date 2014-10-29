15c7198dfd86dbe68a223b84edfd0c3436ea3d48
rockyraccoon20 <tim_montgomery20@yahoo.com>
2014-10-29 14:54:31 -0400

    Added suthentication and column
    added column to schema

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,42 @@
 class UsersController < ApplicationController
+	skip_before_action :require_signin, only: [:new, :create]
+
 	def index
-		
+		@users = User.all
+	end
+
+	def new #sign up
+		 @user = User.new
+	end
+
+	def create
+		@user = User.new(users_params)
+		if @user.save
+			sign_in @user
+			redirect_to @user
+		else
+			render :new
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+	
+	def users_params
+		params.require(:user).permit(
+			:name,
+			:email,
+			:password,
+			:password_confirmation
+		)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'artists/index' => 'artists#index'
+  get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
+  delete '/signout', to: 'sessions#destroy'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
