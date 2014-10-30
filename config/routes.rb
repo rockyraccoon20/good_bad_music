59a27b0c0c759cf9cbc3152f96c4fc82f8715336
rockyraccoon20 <tim_montgomery20@yahoo.com>
2014-10-30 10:31:40 -0400

    Removed artists routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,12 @@ Rails.application.routes.draw do
   
   resources :users
   resources :sessions, only: [:new, :create]
-  resources :artists, only: [:create, :destroy]
   # You can have the root of your site routed with "root"
   root 'users#index'
  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get '/artists', to: 'artists#index'
   get '/signup', to: 'users#new'
   get '/signin', to: 'sessions#new'
   delete '/signout', to: 'sessions#destroy'
